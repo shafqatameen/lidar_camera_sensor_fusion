3c45b5b3607c4070cf29ab636396ee54d9c03055
RationalStudent <shafqatameen316@gmail.com>
2025-02-18 01:09:23 +0530

    i understands pathlib for imgs

---
+++ 2pathlib_img.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+from pathlib import Path
+
+# Get all PCD files from the velodyne directory
+img_files = sorted(Path("data/img").glob("*.png"))
+
+# Check if files exist
+if not img_files:
+    print("No .png files found in 'data/img/'")
+else:
+
+    for file in img_files:
+       img=cv.imread(str(file))
+       print(f"Showing: {file.name}")
+       cv.imshow("Image",img)
+       cv.waitKey(110)
+       cv.destroyAllWindows()
+
